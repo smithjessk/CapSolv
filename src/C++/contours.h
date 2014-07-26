48582804ddf85c806bfe731159dabbd98dd420bc
Jonathan Claus <jonathanmclaus@gmail.com>
2014-07-26 14:52:49 -0400

    Updating contour algorithm

--- src/C++/contours.h
+++ src/C++/contours.h
@@ -1,10 +1,10 @@
 #include <armadillo>
 #include <opencv2/core/core.hpp>
 
-#ifndef Contours
-#define Contours
-
 typedef arma::Mat<unsigned char> cmat;
+typedef arma::Col<unsigned char> cvec;
+
+using namespace arma;
 
 class Contours;
 
@@ -15,7 +15,7 @@ class Contours {
   cmat image_;
 
   // The co-ordinates of the corners of the bounding box for each contour.
-  umat corners;
+  umat corners_;
 
   // Dimensions of the image upon removal of whitespace.
   unsigned int height_, width_;
@@ -30,10 +30,117 @@ class Contours {
   uvec map_;
 
  public:
-  Contours(cv::Mat image);
+  Contours(cv::Mat image) {
+    cout << "Constructing Contours" << endl;
+    cout << "Cols: " << image.cols << " Rows: " << image.rows << endl;
+    cmat arma(image.data, image.cols, image.rows, false, false);
+    cout << "Converted Mat to cmat" << endl;
+    arma = arma.t();
+    cout << "Cols: " << arma.n_cols << " Rows: " << arma.n_rows << endl;
+    uvec rows = find(sum(255 - arma, 1) != 0);
+    uvec cols = find(sum(255 - arma, 0) != 0);
+    // cout << rows << endl;
+    // cout << cols << endl;
+    if (rows.n_elem == 0)
+      throw 1;
+    cout << rows.n_elem << " " << cols.n_elem << endl;
+    cout << rows(0) << endl;
+    cout << rows(rows.n_elem - 1) << endl;
+    cout << cols(0) << endl;
+    cout << cols(cols.n_elem - 1) << endl;
+    image_ = arma.submat(rows(0), rows(rows.n_elem - 1),
+                         cols(0), cols(cols.n_elem - 1));
+    cout << "Image cropped" << endl;
+    cout << "Cols: " << arma.n_cols << " Rows: " << arma.n_rows << endl;
 
-  cmat GetContour(int counter) const;
 
- }
+    // The current number of contours discovered.
+    unsigned char contour_counter = (image_(0, 0) == 0) ? 1 : 0;
 
-#endif
+    // List contours that were merged into another.
+    cvec missing(256);
+
+    // Finding contours in the first column.
+    for (int r = 1; r < image_.n_rows; r++)
+      if (image_(r, 0) != 255)
+	if (image_(r - 1, 0) != 255)
+	  image_(r, 0) = image_(r - 1, 0);
+	else
+	  image_(r, 0) = contour_counter++;
+
+    // Finding contours in the first row.
+    for (int c = 1; c < image_.n_cols; c++)
+      if (image_(0, c) != 255)
+	if (image_(0, c - 1) != 255)
+	  image_(0, c) = image_(0, c - 1);
+	else
+	  image_(0, c) = contour_counter++;
+
+    // Finding contours everywhere else
+    for (int c = 1; c < image_.n_cols; c++) {
+      for (int r = 1; r < image_.n_rows; r++) {
+	if (image_(r, c) != 255) {
+	  if (image_(r - 1, c) != 255 && image_(r, c - 1) != 255) {
+	    // Meeting of contours.
+	    if (image_(r - 1, c) == image_(r, c - 1)) {
+	      image_(r, c) == image_(r - 1, c);
+	    } else {
+	      // Merging of contours.
+	      image_(r, c) = image_(r - 1, c);
+	      image_(span(0, r - 1), c).transform([&](unsigned char val) {
+		return (val == image_(r, c - 1)) ? image_(r - 1, c) : val;
+	      });
+	      image_.cols(0, c - 1).transform([&](unsigned char val) {
+		return (val == image_(r, c - 1)) ? image_(r - 1, c) : val;
+	      });
+	      missing.insert_rows(missing.n_elem, image_(r, c-1));
+	    }
+	  } else if (image_(r - 1, c) != 255) {
+	    image_(r, c) = image_(r - 1, c);
+	  } else if (image_(r, c - 1) != 255) {
+	    image_(r, c) = image_(r, c - 1);
+	  } else {
+	    image_(r, c) = contour_counter++;
+	  }
+	}
+      }
+    }
+
+    num_contours_ = contour_counter - missing.n_elem;
+
+    // Creating the map.
+    map_ = uvec(num_contours_);
+
+    // Index of missing currently being considered.
+    int map_index, missing_index;
+    map_index = missing_index = 0;
+    for (int counter = 0; counter < contour_counter; counter++) {
+      if (missing(missing_index) == counter)
+	missing_index++;
+      else
+	map_(map_index++) = counter;
+    }
+
+    // Finding the corners of the bounding box for each contour.
+    corners_ = umat(4, num_contours_);
+
+    for (int counter = 0; counter < num_contours_; counter++) {
+      int index = map_(counter);
+      rows = find(sum(abs(image_ - index), 0) == 0);
+      cols = find(sum(abs(image_ - index), 1) == 0);
+      corners_(0, counter) = rows(0);
+      corners_(1, counter) = rows(rows.n_elem);
+      corners_(2, counter) = cols(0);
+      corners_(3, counter) = cols(cols.n_elem);
+    }
+  }
+
+  cmat GetContour(int counter) const {
+    int index = map_(counter);
+    cmat contour =  image_.submat(corners_(0, index), corners_(1, index),
+				  corners_(2, index), corners_(3, index));
+    return contour.transform([&](unsigned char val) {
+      return (val == index) ? 0 : 255;
+    });
+  }
+ };
--- src/C++/test.cpp
+++ src/C++/test.cpp
@@ -4,18 +4,38 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <armadillo>
-//#include <contours.h>
+
+#include "contours.h"
 
 using namespace cv;
 using namespace arma;
 using namespace std;
+// Applies a threshold that accounts for various intensities
+cv::Mat preProcessing(cv::Mat img) {
+    // Initialization
+    cout << "Applying preprocessing" << endl;
+    cv::Mat mean;
+    cv::Mat stddev;
+
+    // Calculate the mean and standard deviation 
+    cv::meanStdDev(img, mean, stddev);
+    cout << "Mean Intensity: " << mean.at<double>(0) << endl;
+    cout << "Standard Deviation of Intensity: " << stddev.at<double>(0) << endl;
+
+    // Determine lower threshold limit
+    double lowerLimit = mean.at<double>(0) - (1.5 * stddev.at<double>(0));
+
+    // Apply that threshold
+    cv::threshold(img, img, lowerLimit, 255, cv::THRESH_BINARY);
+    return img;
+}
 
 int main(int argc, char** argv) {
-	// Read in image
-	cv::Mat image;
-    image = cv::imread( argv[1], 0);
-
-    cout << "Got done" << endl;
-
-    return 0;
+  cout << "Started" << endl;
+  // Read in image
+  cv::Mat image;
+  image = preProcessing(cv::imread( argv[1], 0));
+  Contours contours (image);
+  cout << "Got done" << endl;
+  return 0;
 }
