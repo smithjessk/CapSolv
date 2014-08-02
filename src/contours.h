b3426604be110c7765c2756d119b281aafb3dfbe
Jonathan Claus <jonathanmclaus@gmail.com>
2014-08-02 16:21:42 -0400

    Updated debug messages

--- src/contours.h
+++ src/contours.h
@@ -39,6 +39,7 @@ class Contours {
     cout << "Cols: " << arma.n_cols << " Rows: " << arma.n_rows << endl;
     uvec rows = find(sum(255 - arma, 1) != 0);
     uvec cols = find(sum(255 - arma, 0) != 0);
+    cout << sum(255 - arma, 0) << endl;
     // cout << rows << endl;
     // cout << cols << endl;
     if (rows.n_elem == 0 || cols.n_elem == 0) {
