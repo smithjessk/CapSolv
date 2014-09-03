054d6485570ff48cd42f1529d960609c8aea9281
Jess <smith.jessk@gmail.com>
2014-09-02 21:24:45 -0400

    Incorrect extension

---
+++ src/parseSymbol.h
@@ -0,0 +1,46 @@
+/**
+ * @author Jess Smith <smith.jessk@gmail.com>
+ * @copyright 2014 Jess Smith
+ * @license MIT
+ */
+
+// Armadillo
+#include <armadillo>
+
+using namespace arma;
+
+class ParseSymbol {
+private:
+	// The id this symbol corresponds to (enumeration found in enum.tsv)
+	int id_,
+
+	// x_ and y_ are the pixel coordinates of the top-left corner of the image
+	// Note that x increases left to right, y increases top to bottom
+	x_, y_, width_, height_,
+
+	// The id_ of the row that this symbol was found in
+	row_;
+
+public:
+	ParseSymbol(int id, int x, int y, int width, int height, int row){
+		// Assigns all passes values to their respective local versions
+		id_ = id;
+
+		x_ = x;
+		y_ = y;
+		width_ = width;
+		height_ = height;
+
+		row_ = row;
+	};
+
+	void SetRow(int row) {
+		row_ = row;
+	}
+
+	// Returns a vector containing the coordinates of the top-left point of the
+	// symbol, its width, and its height
+	arma::ivec GetDimensions() {
+		return ivec({x_, y_, width_, height_});
+	}
+};
